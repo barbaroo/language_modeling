0ec934811088afddb8c86ec768606b7421e6d1e0
jakupsv <jakupsv@setur.fo>
2023-05-30 17:49:18 +0100

    Fix pre-commit configuration errors
    The commit resolves errors in the `.pre-commit-config.yaml` file to ensure proper functionality of the pre-commit hooks. The configuration has been updated to include the necessary hooks and their configurations for the desired checks and code formatting.
    
    Changes:
    - Fixed errors in the `.pre-commit-config.yaml` file
    - Updated the configuration to include required hooks and their configurations
    
    Modified Files:
    - .pre-commit-config.yaml

--- train/dataset.py
+++ train/dataset.py
@@ -1,23 +1,27 @@
 import random
+
 from torch.utils.data import Dataset
 
-#Tokenize text, create pytorch tensors, create masks for MLM and export a pytorch dataset
+
+# Tokenize text, create pytorch tensors, create masks for MLM and export a pytorch dataset
 class Dataset_for_MLM(Dataset):
     def __init__(self, texts, tokenizer, mask_prob):
         self.texts = texts
         self.tokenizer = tokenizer
         self.mask_prob = mask_prob
-        
+
     def __len__(self):
         return len(self.texts)
 
     def __getitem__(self, idx):
         text = self.texts[idx]
-        inputs = self.tokenizer.encode_plus(text, add_special_tokens=True, padding='max_length', max_length=514, truncation=True, return_tensors='pt')
-        input_ids = inputs['input_ids']
-        attention_mask = inputs['attention_mask']
+        inputs = self.tokenizer.encode_plus(
+            text, add_special_tokens=True, padding="max_length", max_length=514, truncation=True, return_tensors="pt"
+        )
+        input_ids = inputs["input_ids"]
+        attention_mask = inputs["attention_mask"]
 
-        #Create masks for masked language modeling
+        # Create masks for masked language modeling
         masked_indices = [i for i, token in enumerate(input_ids[0]) if token != self.tokenizer.pad_token_id]
         num_masked = max(1, int(len(masked_indices) * self.mask_prob))
         masked_indices = random.sample(masked_indices, num_masked)
@@ -27,7 +31,7 @@ class Dataset_for_MLM(Dataset):
             input_ids_masked[0, idx] = self.tokenizer.mask_token_id
 
         return {
-            'input_ids': input_ids.squeeze(),
-            'attention_mask': attention_mask.squeeze(),
-            'labels': input_ids_masked.squeeze()
+            "input_ids": input_ids.squeeze(),
+            "attention_mask": attention_mask.squeeze(),
+            "labels": input_ids_masked.squeeze(),
         }
--- train/trainer.py
+++ train/trainer.py
@@ -1,12 +1,13 @@
-import torch
 import pandas as pd
-from torch.utils.data import Dataset, DataLoader
-from tqdm import tqdm
+import torch
 from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader, Dataset
+from tqdm import tqdm
+
 from train.dataset import Dataset_for_MLM
 
+# The trainer class loads the dataset, trains and tests the model with MLM objective
 
-#The trainer class loads the dataset, trains and tests the model with MLM objective 
 
 class MLMTrainer:
     def __init__(self, model, tokenizer, dataset_path, batch_size, lr, epochs, device, n_examples, split):
@@ -22,13 +23,13 @@ class MLMTrainer:
 
     def load_dataset(self):
         data = pd.read_csv(self.dataset_path)
-        sentences = data['Text'].tolist()[:self.n_examples]
-        
-        #Split sentences into train and test set
-        sentences_train, sentences_test = train_test_split(sentences, test_size= self.split, random_state=42)
+        sentences = data["Text"].tolist()[: self.n_examples]
+
+        # Split sentences into train and test set
+        sentences_train, sentences_test = train_test_split(sentences, test_size=self.split, random_state=42)
         dataset_train = Dataset_for_MLM(sentences_train, self.tokenizer)
         dataset_test = Dataset_for_MLM(sentences_test, self.tokenizer)
-        
+
         return dataset_train, dataset_test
 
     def train(self):
@@ -40,40 +41,38 @@ class MLMTrainer:
         self.model.train()
 
         num_training_steps = self.epochs * len(dataloader)
-        
-        #Training loop
+
+        # Training loop
         for epoch in range(self.epochs):
             loop = tqdm(dataloader, leave=True)
             for batch in loop:
                 optimizer.zero_grad()
-                input_ids = batch['input_ids'].to(self.device)
-                attention_mask = batch['attention_mask'].to(self.device)
-                labels = batch['labels'].to(self.device)
+                input_ids = batch["input_ids"].to(self.device)
+                attention_mask = batch["attention_mask"].to(self.device)
+                labels = batch["labels"].to(self.device)
 
                 outputs = self.model(input_ids, attention_mask=attention_mask, labels=labels)
                 loss = outputs.loss
                 loss.backward()
                 optimizer.step()
 
-                loop.set_description(f'Epoch: {epoch}')
+                loop.set_description(f"Epoch: {epoch}")
                 loop.set_postfix(loss=loss.item())
-                
-    
-    
+
     def test(self):
         self.model.eval()
         dataset_train, dataset_test = self.load_dataset()
         test_dataloader = DataLoader(dataset_test, batch_size=self.batch_size, shuffle=False)
-        
-        #Calculate loss over test set
+
+        # Calculate loss over test set
         with torch.no_grad():
             total_loss = 0
             total_examples = 0
 
             for batch in test_dataloader:
-                input_ids = batch['input_ids'].to(self.device)
-                attention_mask = batch['attention_mask'].to(self.device)
-                labels = batch['labels'].to(self.device)
+                input_ids = batch["input_ids"].to(self.device)
+                attention_mask = batch["attention_mask"].to(self.device)
+                labels = batch["labels"].to(self.device)
 
                 outputs = self.model(input_ids, attention_mask=attention_mask, labels=labels)
                 loss = outputs.loss
@@ -82,7 +81,4 @@ class MLMTrainer:
                 total_examples += batch_size
 
         avg_loss = total_loss / total_examples
-        print(f'Average Test Loss: {avg_loss}')
-
-
-
+        print(f"Average Test Loss: {avg_loss}")
