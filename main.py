633dabd41ad3283d9cc52211676e9f114c5e2db5
jakupsv <jakupsv@setur.fo>
2023-05-30 17:26:46 +0100

    Add config.yaml and update main.py for model training
    The commit adds the configuration file config.yaml, which includes the necessary parameters for the model training process. Additionally, the main.py file has been updated to load the YAML configuration, initialize the model and tokenizers, and start the training process using the specified parameters.
    
    Changes:
    - Added config.yaml
    - Updated main.py to include the loading of the YAML configuration, model and tokenizer initialization, and starting the training process
    
    Modified Files:
    - config.yaml
    - main.py

--- main.py
+++ main.py
@@ -1,15 +1,38 @@
+import torch
+import yaml
 from train.trainer import MLMTrainer
 from transformers import AutoModelForMaskedLM, AutoTokenizer
-from base import MODEL_NAME
 
-#Initialize model and tokenizers
-tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
-model = AutoModelForMaskedLM.from_pretrained(MODEL_NAME)
+def main():
+    # Load YAML configuration file
+    with open('config.yaml', 'r') as f:
+        config = yaml.safe_load(f)
+    
+    # Access parameters from the configuration file
+    model_name = config['MODEL_NAME']
+    path_to_data = config['PATH_TO_DATA']
+    # output_dir = config['OUTPUT_DIR']  # Uncomment if needed
 
-#Train model
-trainer = MLMTrainer(model, tokenizer)
-trainer.train()
+    batch_size = config['BATCH_SIZE']
+    train_test_split = config['TRAIN_TEST_SPLIT']
+    learning_rate = config['LEARNING_RATE']
+    epochs = config['EPOCHS']
+    max_tokens = config['MAX_TOKENS']
+    number_examples = config['NUMBER_EXAMPLES']
+    device = torch.device(config['DEVICE'])
+    mask_probability = config['MASK_PROBABILITY']
+    
+    # Initialize model and tokenizers
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForMaskedLM.from_pretrained(model_name)
+    
+    # Train model
+    trainer = MLMTrainer(model, tokenizer)
+    trainer.train()
+    
+    # Test model
+    trainer.test()
 
-#Test model
-trainer.test()
 
+if __name__ == '__main__':
+    main()
